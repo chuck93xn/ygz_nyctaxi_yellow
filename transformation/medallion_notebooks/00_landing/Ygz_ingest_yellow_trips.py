598f37e7053ff3b5b96cc3454ff33c2093497922
Chuck Wang <chuck93xn@gmail.com>
2026-07-06 23:30:31 +1000

    modify the code with functions

--- transformation/medallion_notebooks/00_landing/Ygz_ingest_yellow_trips.py
+++ transformation/medallion_notebooks/00_landing/Ygz_ingest_yellow_trips.py
@@ -1,20 +1,27 @@
 # Databricks notebook source
-import urllib.request
 import os
+import sys
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2] 
+if str(PROJECT_ROOT) not in sys.path:
+    sys.path.append(str(PROJECT_ROOT))
+
+import urllib.request
 import shutil
 from datetime import datetime
 from datetime import date, datetime, timezone
 from dateutil.relativedelta import relativedelta
+from modules.data_loader_landing.file_downloader import download_file
+from modules.utils.date_utils import get_target_yyyymm
 
 # COMMAND ----------
 
-# Obtains the year-month for 2 months prior to the current month in yyyy-MM format
-two_months_ago = date.today() - relativedelta(months=2)
-date_ingest = two_months_ago.strftime("%Y-%m")
+formatted_date = get_target_yyyymm(2)
 
-dir_path = f"/Volumes/nyctaxi_ygz/00_landing/data_sources/nyctaxi_yellow/{date_ingest}"
+dir_path = f"/Volumes/nyctaxi_ygz/00_landing/data_sources/nyctaxi_yellow/{formatted_date}"
 
-local_path = f"{dir_path}/yellow_tripdata_{date_ingest}.parquet"
+local_path = f"{dir_path}/yellow_tripdata_{formatted_date}.parquet"
 
 # COMMAND ----------
 
@@ -26,11 +33,9 @@ try:
     print("File already downloaded, aborting downstream tasks")
 except:
     try:
-        url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{date_ingest}.parquet"
-        response = urllib.request.urlopen(url)
-        os.makedirs(dir_path, exist_ok=True)
-        with open(local_path, 'wb') as f:
-            shutil.copyfileobj(response, f)
+        url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{formatted_date}.parquet"
+
+        download_file(url, dir_path, local_path)
         
         # Set continue_downstream to yes if the file was loaded
         dbutils.jobs.taskValues.set(key="continue_downstream", value="yes")
@@ -40,47 +45,3 @@ except:
         # Set continue downstream to no if the file was not loaded
         dbutils.jobs.taskValues.set(key="continue_downstream", value="no")
         print(f"File download failed: {str(e)}")
-
-# COMMAND ----------
-
-# MAGIC %skip
-# MAGIC
-# MAGIC # Obtains the year-month for 2 months prior to the current month in yyyy-MM format
-# MAGIC two_months_ago = date.today() - relativedelta(months=2)
-# MAGIC formatted_date = two_months_ago.strftime("%Y-%m")
-# MAGIC
-# MAGIC # Define the local directory for this date's data
-# MAGIC dir_path = f"/Volumes/nyctaxi/00_landing/data_sources/nyctaxi_yellow/{formatted_date}"
-# MAGIC
-# MAGIC # Define the full path for the downloaded file
-# MAGIC local_path = f"{dir_path}/yellow_tripdata_{formatted_date}.parquet"
-# MAGIC
-# MAGIC try:
-# MAGIC     # Check if the file already exists
-# MAGIC     dbutils.fs.ls(local_path)
-# MAGIC
-# MAGIC     # If the file already exists then set continue_downstream to no
-# MAGIC     dbutils.jobs.taskValues.set(key="continue_downstream", value="no")
-# MAGIC     print("File already downloaded, aborting downstream tasks")
-# MAGIC except:
-# MAGIC     try:
-# MAGIC         # Construct the URL for the Parquet file corresponding to this month
-# MAGIC         url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{formatted_date}.parquet"
-# MAGIC
-# MAGIC         # Open a connection and stream the remote file
-# MAGIC         response = urllib.request.urlopen(url)
-# MAGIC
-# MAGIC         # Create the local directory for this date's data
-# MAGIC         os.makedirs(dir_path, exist_ok=True)
-# MAGIC
-# MAGIC         # Save the streamed content to the local file in binary mode
-# MAGIC         with open(local_path, 'wb') as f:
-# MAGIC             shutil.copyfileobj(response, f)  # Copy data from response to file
-# MAGIC         
-# MAGIC         # Set continue_downstream to yes if the file was loaded
-# MAGIC         dbutils.jobs.taskValues.set(key="continue_downstream", value="yes")
-# MAGIC         print("File succesfully uploaded in current run")
-# MAGIC     except Exception as e:
-# MAGIC         # Set continue downstream to no if the file was not loaded
-# MAGIC         dbutils.jobs.taskValues.set(key="continue_downstream", value="no")
-# MAGIC         print(f"File download failed: {str(e)}")
--- transformation/medallion_notebooks/01_bronze/Ygz_yellow_trips_raw_ingest.py
+++ transformation/medallion_notebooks/01_bronze/Ygz_yellow_trips_raw_ingest.py
@@ -1,21 +1,29 @@
 # Databricks notebook source
+import os
+import sys
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2] 
+if str(PROJECT_ROOT) not in sys.path:
+    sys.path.append(str(PROJECT_ROOT))
+
+
 from datetime import date, datetime, timezone
 from pyspark.sql.functions import current_timestamp
 from dateutil.relativedelta import relativedelta
-
+from modules.utils.date_utils import get_target_yyyymm
+from modules.transformation_layers.metadata import add_processed_timestamp
 # COMMAND ----------
 
-# Obtains the year-month for 2 months prior to the current month in yyyy-MM format
-two_months_ago = date.today() - relativedelta(months=2)
-date_ingest = two_months_ago.strftime("%Y-%m")
-file_path = f"/Volumes/nyctaxi_ygz/00_landing/data_sources/nyctaxi_yellow/{date_ingest}/"
+formatted_date = get_target_yyyymm(2)
+file_path = f"/Volumes/nyctaxi_ygz/00_landing/data_sources/nyctaxi_yellow/{formatted_date}/"
 
 # COMMAND ----------
 
 # Read files for the specified month from the landing directory into a DataFrame
 df = spark.read.format("parquet").load(file_path)
 
-df = df.withColumn('processing_timestamp', current_timestamp())
+df = add_processed_timestamp(df)
 
 # COMMAND ----------
 
